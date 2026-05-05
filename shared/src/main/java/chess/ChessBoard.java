63d4a84021bc493062017f36dcee723d9ed63f13
Matthew Clarkson <matt77@byu.edu>
2026-05-04 18:10:30 -0600

    hash codes, started reset board, made a cool toString for the chess board

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,10 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -41,6 +46,61 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        // set up for white side
+        int pwnCntr = 1;
+        while (pwnCntr <= 8) {
+            ChessPosition newPawnPosition = new ChessPosition(2, pwnCntr);
+            ChessPiece newPawnPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            addPiece(newPawnPosition, newPawnPiece);
+            pwnCntr++;
+        }
+
     }
+
+    @Override
+    public String toString() {
+        List<String> boardPieces = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                boardPieces.add("|");
+                if (squares[i][j] == null) {
+                    boardPieces.add(" ");
+                } else {
+                    if (squares[i][j].getTeamColor() == ChessGame.TeamColor.WHITE) {
+                        if (squares[i][j].getPieceType() == ChessPiece.PieceType.PAWN) {
+                            boardPieces.add("P");
+                        }
+                        else if (squares[i][j].getPieceType() == ChessPiece.PieceType.KING) {
+                            boardPieces.add("K");
+                        }
+                        else if (squares[i][j].getPieceType() == ChessPiece.PieceType.QUEEN) {
+                            boardPieces.add("Q");
+                        }
+                        else boardPieces.add("X");
+                    }
+                    else boardPieces.add("X");
+                }
+            }
+            boardPieces.add("\n");
+        }
+
+
+
+        return boardPieces.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
+    }
+
 }
