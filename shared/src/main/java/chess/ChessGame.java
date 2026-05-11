a522e5f5c99ad95930485558f7edaf1cb603ee4d
Matthew Clarkson <matt77@byu.edu>
2026-05-11 14:54:15 -0600

    isInStalemate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -185,7 +185,23 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("yo mama");
+        if (!this.isInCheck(teamColor)) { // if the king is currently in check
+            for (int i = 1; i < 9; i++) {
+                for (int j = 1; j < 9; j++) {
+                    ChessPosition currentPosition = new ChessPosition(i,j);
+                    ChessPiece currentPiece = gameboard.getPiece(currentPosition);
+                    if (currentPiece != null) {
+                        if (currentPiece.getTeamColor() == teamColor) {
+                            if (!this.validMoves(currentPosition).isEmpty()) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
     }
 
     /**
